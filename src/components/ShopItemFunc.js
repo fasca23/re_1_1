dfea25b74ae498e8c89acd1bd951add769e06e59
V.Kamenev <110154088+fasca23@users.noreply.github.com>
2023-11-24 17:44:38 +0300

    add cra

---
+++ src/components/ShopItemFunc.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ShopItemFunc(props) {
+  const {item} = props;
+
+  return (
+    <div className="main-content">
+      <span>Функциональный компонент</span>
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div className="description">{item.descriptionFull}</div>
+      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{item.currency}{item.price.toFixed(2)}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
+}
+
+// Проверили типы
+ShopItemFunc.propTypes = {
+  item: PropTypes.object.isRequired
+}
+  
+export default ShopItemFunc;
